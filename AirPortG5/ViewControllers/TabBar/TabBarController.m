ad8f2eaa8ecccfdd5e4b732822004798281ab5d8
Konstantin <kynadonenko@gmail.com>
2021-07-05 20:50:55 +0300

    done

---
+++ AirPortG5/ViewControllers/TabBar/TabBarController.m
@@ -0,0 +1,51 @@
+//
+//  TabBarController.m
+//  AirPortG5
+//
+//  Created by Константин Надоненко on 05.07.2021.
+//
+
+#import "TabBarController.h"
+
+@interface TabBarController ()
+
+@end
+
+@implementation TabBarController
+
+- (instancetype)init {
+
+    self = [super initWithNibName:nil bundle:nil];
+
+    if (self) {
+        self.viewControllers = [self createViewControllers];
+        self.tabBar.tintColor = [UIColor blueColor];
+    }
+
+    return self;
+}
+
+
+- (NSArray<UIViewController*> *)createViewControllers {
+
+    NSMutableArray<UIViewController*> *controllers = [NSMutableArray new];
+
+    MainViewController *mainViewController = [[MainViewController alloc] init];
+    mainViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle: @"Поиск" image: [UIImage imageNamed: @"search"] selectedImage: [UIImage imageNamed: @"search"]];
+    UINavigationController *mainNavigationController = [[UINavigationController alloc] initWithRootViewController: mainViewController];
+    [controllers addObject: mainNavigationController];
+
+    MapViewController *mapViewController = [[MapViewController alloc] init];
+    mapViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle: @"Карта цен" image: [UIImage imageNamed: @"map"] selectedImage: [UIImage imageNamed: @"map"]];
+    UINavigationController *mapNavigationController = [[UINavigationController alloc] initWithRootViewController: mapViewController];
+    [controllers addObject:mapNavigationController];
+
+    FeedViewController *feedViewController = [[FeedViewController alloc] init];
+    feedViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle: @"Новости" image: [UIImage imageNamed: @"news"] selectedImage: [UIImage imageNamed: @"news"]];
+    UINavigationController *feedNavigationController = [[UINavigationController alloc] initWithRootViewController: feedViewController];
+    [controllers addObject:feedNavigationController];
+
+    return controllers;
+}
+
+@end
